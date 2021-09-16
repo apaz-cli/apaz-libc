417f9457c9c034094b9656aa5e405e4015c7ebdb
apaz-cli <aarpazdera@gmail.com>
2021-09-16 18:07:48 -0500

    Updated install script since variadic.h moved.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sudo cp -r list.h/        /usr/include/
-sudo cp -r memdebug.h/    /usr/include/
-sudo cp -r apaz-string.h/ /usr/include/
-sudo cp -r threadpool.h/  /usr/include/
-sudo cp    apaz-libc.h    /usr/include/apaz-libc.h 
+sudo cp -r list.h/          /usr/include/
+sudo cp -r memdebug.h/      /usr/include/
+sudo cp -r apaz-string.h/   /usr/include/
+sudo cp -r threadpool.h/    /usr/include/
+sudo cp    apaz-libc.h      /usr/include/apaz-libc.h 
+sudo cp    apaz-variadic.h  /usr/include/apaz-variadic.h
